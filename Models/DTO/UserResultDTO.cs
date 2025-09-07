b7a011207553b14c85cd710b5ac885d0001bb15f
tbalma <balmaceda0205@gmail.com>
2025-09-06 23:04:07 -0300

    Agrego models al proyecto

---
+++ Models/DTO/UserResultDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGS_Models.DTO
+{
+    public class UserResultDTO
+    {
+        public bool Result { get; set; }
+        public string Message {get;set;}
+    }
+}
