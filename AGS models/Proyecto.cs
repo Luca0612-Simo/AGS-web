7cdc42ea043af449b4f1e695661991f2c33144f7
Luca <lucasimonetti0612@gmail.com>
2025-11-14 17:16:15 -0300

    feat: creat ABM of Project

--- AGS models/Proyecto.cs
+++ AGS models/Proyecto.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema; // <-- AGREGÁ ESTE
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
